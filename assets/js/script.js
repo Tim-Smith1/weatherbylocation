900677b9e39ab698043953c21af8b570e1774562
Tim Smith <meckaleck@hotmail.com>
2023-02-05 23:40:01 -0600

    2/5 query is now working via input box. todos work on weather icons and then tackle local storage for the buttons

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,45 +8,52 @@
 
 var btnGet = document.getElementById('btn-get')
 var inputGet = document.getElementById('input-get')
+var cityName = ''; 
 
 console.log(btnGet);
 
+// function searchResult() {
+//     inputGet.innerHTML = search.value;
+// }
+
 function searchResult() {
-    inputGet.innerHTML = search.value;
-}
-
-
-inputGet.addEventListener('click', searchResult);
-
-
-var cityName = '';
-cityName = 'new york';
-
-
-
-var endUrl = ('https://api.openweathermap.org/data/2.5/forecast?q=' + cityName + '&units=imperial&appid=b3116830bb7295ca81c53365dc533b51');
-
-
-//console.log(endUrl);
-// clouds clear snow rain
-
-//cityName = 'el paso';
-
+    console.log(inputGet.value)
+    //var city = inputGet.value;
+    cityName = inputGet.value;
+    var endUrl = ('https://api.openweathermap.org/data/2.5/forecast?q=' + cityName + '&units=imperial&appid=b3116830bb7295ca81c53365dc533b51');
 
 fetch(endUrl)
-  .then(function (response) {
+.then(function (response) {
     return response.json();
   })
-  .then(function (data) {
+.then(function (data) {
     console.log('Weather \n----------');
     console.log(data);
     console.log(data.city.name);
-    
+
+function weatherIcon() {
+    var weatherIcon ='';
+
+    if ((data.list[0].weather[0].main) === 'Clouds') {
+        weatherIcon = '&#9749;';
+    } else if ((data.list[0].weather[0].main) === 'Clear') {
+        weatherIcon = '&#9728;';
+    } else if ((data.list[0].weather[0].main) === 'Snow') {
+        weatherIcon = '&#9731;';
+    } else if ((data.list[0].weather[0].main) === 'Rain') {
+        weatherIcon = '&#9730;';
+    } else {
+        weatherIcon = '&#9749;';
+    }
+}
+
+weatherIcon.weatherIcon ='';
+
     //main panel showing selected city
     document.getElementById('selected-city').innerHTML = (data.city.name + ' (' + (dayjs().format('MM/DD/YYYY')) + ')');
     //Current temp day0
     document.getElementById('currnet-temp').innerHTML=('Temp: ' +(data.list[0].main.temp + 'ºF'));
-    document.getElementById('current-icon').innerHTML=('&#9749;' + (data.list[0].weather[0].main));
+    document.getElementById('current-icon').innerHTML=(weatherIcon + (data.list[0].weather[0].main));
     document.getElementById('current-wind').innerHTML=('Wind: ' + (data.list[0].wind.speed) + ' MPH');
     document.getElementById('current-humidity').innerHTML=('Humidity: ' + (data.list[0].main.humidity) + '%');
     
@@ -82,6 +89,37 @@ fetch(endUrl)
     document.getElementById('day5-icon').innerHTML = ('&#9749;' + (data.list[39].weather[0].main));
     document.getElementById('day5-wind').innerHTML = ('Wind: ' + (data.list[39].wind.speed)  + ' MPH');
     document.getElementById('day5-humidity').innerHTML = ('Humidity: ' + (data.list[39].main.humidity)  + ' %');
+      
+  });
+}
+
+
+btnGet.addEventListener('click', searchResult);
+
+
+//var cityName = '';
+//cityName = 'new york';
+
+
+
+// var endUrl = ('https://api.openweathermap.org/data/2.5/forecast?q=' + cityName + '&units=imperial&appid=b3116830bb7295ca81c53365dc533b51');
+
+
+//console.log(endUrl);
+// clouds  &#9729;
+// clear   &#9728;
+// snow    &#9731;
+// rain    &#9730;
+
+//cityName = 'el paso';
+
+
+
+
+
+  // list.0.main.temp
+  // list[0].main.temp
+
 
 
     // TODO: Loop through the response
@@ -91,9 +129,4 @@ fetch(endUrl)
     // }
     // if (data[i].city.coord.lat >= 20){
     //   console.log(data[i].city);
-    // }  
-  });
-
-
-  // list.0.main.temp
-  // list[0].main.temp
+   
